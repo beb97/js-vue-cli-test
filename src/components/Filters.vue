7df99296d857a5a22efde55427e304cb1df0d34e
beb97 <beb97@hotmail.com>
2021-05-01 09:56:55 +0200

    MaJ du site

---
+++ src/components/Filters.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="container flex vertical">
+    <div class="name">{{name}}</div>
+  </div>
+<!--<div>{{nameRef}}</div>-->
+</template>
+
+<!--description-->
+<!--name-->
+<!--nameRef-->
+
+<script>
+export default {
+name: "Rarity.vue",
+  props: {
+    rarity: Object
+  },
+  data() {
+    return {
+      name: this.rarity.name,
+      nameRef: this.rarity.nameRef,
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .container {
+    border: 1px solid grey;
+  }
+  .name {
+    font-weight: bold;
+  }
+</style>
